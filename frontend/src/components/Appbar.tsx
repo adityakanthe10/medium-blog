50963cec4399663f9d82a77fb4c812952c7c43b1
adityakanthe10 <adityakanthe10@gmail.com>
2025-04-03 22:56:07 +0530

    small fixes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import { Avatar } from "./BlogCard";
+import { assets } from "../assets/assets";
 
 export const Appbar = () => {
   return (
     <div className="border-b flex justify-between px-10 py-4">
       <Link to={"/blogs"}>
-        <div className="flex flex-col justify-center cursor-pointer">Easy</div>
+        <div className="flex flex-col justify-center items-center cursor-pointer">
+          <img src={assets.logo} className="h-10 " />
+        </div>
       </Link>
       <div className="flex">
         <Link to={"/publish"}>
@@ -13,7 +16,7 @@ export const Appbar = () => {
             type="button"
             className="mr-8 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
           >
-            New
+            Publish
           </button>
         </Link>
 
